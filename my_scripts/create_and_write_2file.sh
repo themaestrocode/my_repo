83e7d9cf5d288e37bed55a0005e8aad9f0570f87
victor <victorsoderu@gamil.com>
2023-02-10 14:22:10 +0000

    added a script to create and edit files using either vi or emacs

--- my_scripts/create_and_write_2file.sh
+++ my_scripts/create_and_write_2file.sh
@@ -3,19 +3,28 @@ echo "Enter the name of your file: "
 read file_name
 
 touch $file_name
-echo "This is an empty file, would you like to write to it?\nPress 1 to use vi\nPress 2 to use emacs\nPress any other number to save as an empty file"
-read opcode
-if ["$opcode" = "1"]; then
-       vi $file_name
-       echo "$file_name saved successfully"
-else if ["$opcode" = "2"]; then
-emacs $file_name
-echo "$file_name saved successfully!"
+
+read -p "This is an empty file. Would you like to write to it? Enter y or n" input
+
+if [ "$input" = "y" ]; then
+	read -p "enter vi to use vi, enter emacs to use emacs" editor
+
+	if [ "$editor" = "vi" ]; then
+		vi $file_name
+		echo "$file_name saved successfully"
+	elif [ "$editor" = "emacs" ]; then
+		emacs $file_name
+		echo "$file_name saved successfully"
+	else
+		echo "wrong input 1"
+	fi
+
+elif [ "$input" = "n" ]; then
+	echo "$file_name saved successfully!"
+
 else
-	echo "file saved!"
+	echo "wrong input 2"
 
 fi
 
 chmod 777 $file_name
-
-
